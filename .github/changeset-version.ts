0b08041ea1f7ef425cab67a11fe8cb0c2a4316b3
Rohan Mukherjee <rmukherjee@cloudflare.com>
2026-04-06 14:29:02 +0530

    chore: add release workflow, bonk workflow, issue template, and tsx

---
+++ .github/changeset-version.ts
@@ -0,0 +1,12 @@
+import { execSync } from "node:child_process";
+
+// This script is used by the release workflow to update package versions.
+// The standard step is only to run `changeset version` but this does not
+// update the lockfile. So we also run `pnpm install` to keep it in sync.
+// See https://github.com/changesets/changesets/issues/421.
+execSync("pnpm changeset version", {
+	stdio: "inherit",
+});
+execSync("pnpm install --no-frozen-lockfile", {
+	stdio: "inherit",
+});
